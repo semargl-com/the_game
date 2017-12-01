c9e69da69b855d673aa45b0807370fc5df806649
Oleksii Konokhov <Oleksii_Konokhov@epam.com>
2017-12-01 16:20:09 +0200

    generate libgdx project

---
+++ android/src/com/thegame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.thegame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.thegame.TheGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TheGame(), config);
+	}
+}
